360e98d6bc8cfdf92fbded6891020e1b5c3f4d55
nayakashok2010 <nayakashok2010@gmail.com>
2026-06-29 16:28:34 +0530

    code test 1

--- src/main/java/com/tlm/cicd/springboot_docker_ci_demo/DemoController.java
+++ src/main/java/com/tlm/cicd/springboot_docker_ci_demo/DemoController.java
@@ -13,15 +13,23 @@ import java.util.List;
 public class DemoController {
     @Autowired
     JdbcTemplate jdbcTemplate;
-    //String password = "Admin@123";
-    //String api_Key = "sk_live_xxxxxxxxx";
+    String password = "Admin@123";
+    String apikey = "tlm_demo_xxxxxxxxx";
     @GetMapping("/")
     public String demo() {
 
         return "Hi TLM team, welcome to vulnerability demo";
     }
 
+    @GetMapping("/users")
+    public List<Employee> getUsers(String username) {
 
+        String sql = "SELECT * FROM employee WHERE name='" + username + "'";
+        System.out.println("sql query to execute : " + sql);
+        return jdbcTemplate.query(sql,
+                new BeanPropertyRowMapper<>(Employee.class));
+
+    }
 
     @GetMapping("/emp")
     public List<Employee> getEmployees(String username) {
